eb79ea2358cb5b8237cf32ab3c31f4329f5bfdc9
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-04 10:45:57 -0500

    Python Example 3

--- Big_O/examples.py
+++ Big_O/examples.py
@@ -12,22 +12,36 @@ import timeit
 
 #   print("%s, %s" % (sum1, product))
 
-# # 0.02430102601647377
 # t = timeit.timeit('example1([1,2,3,4,5,6,7,8,9,10])', setup='from __main__ import example1', number=1000)
 # print(t)
+# # 0.02430102601647377
+# # O(N)
 
-# EXAMPLE 2
-def example2(array):
-  for num in array:
-    for nums in array:
-      print("%s, %s" % (num, nums))
+# # EXAMPLE 2
+# def example2(array):
+#   for num in array:
+#     for nums in array:
+#       print("%s, %s" % (num, nums))
 
-# 1.740927321021445
-t = timeit.timeit('example2([1,2,3,4,5,6,7,8,9,10])', setup='from __main__ import example2', number=1000)
-print(t)
+# t = timeit.timeit('example2([1,2,3,4,5,6,7,8,9,10])', setup='from __main__ import example2', number=1000)
+# print(t)
+# # 1.740927321021445
+# # O(N^2)
 
 # EXAMPLE 3
+def example3(array):
+  for x in range(len(array)):
+    for y in range(x+1, len(array)):
+      print("%s, %s" % (x, y))
+
+t = timeit.timeit('example3([1,2,3,4,5,6,7,8,9,10])', setup='from __main__ import example3', number=1000)
+print(t)
+# 0.7609443860128522
+# O(N^2)
+
 # EXAMPLE 4
+
+
 # EXAMPLE 5
 # EXAMPLE 6
 # EXAMPLE 7
