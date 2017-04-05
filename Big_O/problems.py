3fab15eb3b34fb076c8c8ca349bca9cd619bdfae
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-04 20:15:36 -0500

    Python Problem 4

--- Big_O/problems.py
+++ Big_O/problems.py
@@ -20,13 +20,25 @@
 # print(compute(3, 3))
 # # O(N)
 
-# PROBLEM 3
-def remainder(a, b):
-  if b < 0:
-    return -1
-  else:
-    div = int(a / b)
-    return a - (div * b)
+# # PROBLEM 3
+# def remainder(a, b):
+#   if b < 0:
+#     return -1
+#   else:
+#     div = int(a / b)
+#     return a - (div * b)
 
-print(remainder(33, 6))
-# O(1)
+# print(remainder(33, 6))
+# # O(1)
+
+# PROBLEM 4
+def divison(a, b):
+  count = 0
+  sum = b
+  while (sum <= a):
+    sum += b
+    count += 1
+  return count
+
+print(divison(100, 10))
+# O(N)
