36fb06d9014fc9f87703e763e0628e40e33447f3
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-04 20:13:41 -0500

    Python Problem 2

--- Big_O/problems.py
+++ Big_O/problems.py
@@ -1,9 +1,21 @@
-# PROBLEM 1
-def product(a, b):
-  sum = 0
-  for x in range(b):
-    sum += a
-  return sum
+# # PROBLEM 1
+# def product(a, b):
+#   sum = 0
+#   for x in range(b):
+#     sum += a
+#   return sum
 
-print(product(8,10))
+# print(product(8,10))
+# # O(N)
+
+# PROBLEM 2
+def compute(a, b):
+  if b < 0:
+    return 0 # error
+  elif b == 0:
+    return 1
+  else:
+    return a * compute(a, b - 1)
+
+print(compute(3, 3))
 # O(N)
