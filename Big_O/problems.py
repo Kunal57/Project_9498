47f5252b9f997532ebd595efa551f2bfc0684237
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-04 20:14:53 -0500

    Python Problem 3

--- Big_O/problems.py
+++ Big_O/problems.py
@@ -8,14 +8,25 @@
 # print(product(8,10))
 # # O(N)
 
-# PROBLEM 2
-def compute(a, b):
-  if b < 0:
-    return 0 # error
-  elif b == 0:
-    return 1
-  else:
-    return a * compute(a, b - 1)
+# # PROBLEM 2
+# def compute(a, b):
+#   if b < 0:
+#     return 0 # error
+#   elif b == 0:
+#     return 1
+#   else:
+#     return a * compute(a, b - 1)
 
-print(compute(3, 3))
-# O(N)
+# print(compute(3, 3))
+# # O(N)
+
+# PROBLEM 3
+def remainder(a, b):
+  if b < 0:
+    return -1
+  else:
+    div = int(a / b)
+    return a - (div * b)
+
+print(remainder(33, 6))
+# O(1)
