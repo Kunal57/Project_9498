6618b9d30fa3aeb063cd6af6548d4f175cf89a1d
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-11 16:40:38 -0500

    Write function with O(n) runtime

--- Arrays/first_nonrepeated_character.py
+++ Arrays/first_nonrepeated_character.py
@@ -1,14 +1,28 @@
 # Write an efficient function to find the first non-repeated character in a string. For instance, the first non-repeated character in "total" is "o" and the first non-repeated character in "tweeter" is "r". Discuss the efficiency of your algorithm.
 
+# def firstNonRepeat(string):
+#   for x in range(len(string)):
+#     for y in range(len(string)):
+#       if string[x] == string[y] and x != y:
+#         break
+#       elif y + 1 == len(string):
+#         return string[x]
+
+
+# print(firstNonRepeat("teeter"))
+# # O(N^2)
+
 def firstNonRepeat(string):
+  letters = {}
   for x in range(len(string)):
-    for y in range(len(string)):
-      if string[x] == string[y] and x != y:
-        break
-      elif y + 1 == len(string):
-        return string[x]
+    letters[string[x]] = letters.get(string[x], 0) + 1
+
+  for x in range(len(string)):
+    if letters[string[x]] == 1:
+      return string[x]
+
+  return None
 
 
 print(firstNonRepeat("teeter"))
-# O(N^2)
-
+# O(2N)
