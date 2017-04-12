81bc03194be2108f79c0cf0988cd27b0798433d8
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-12 14:33:39 -0500

    Remove Specified Characters in O(n) runtime

---
+++ Arrays/remove_specified_characters.py
@@ -0,0 +1,15 @@
+# Write an efficient function that deletes characters from an ASCII string. Use the prototype:
+# string removeChars(string str, string remove)
+# where any characters existing in remove must be deleted from str. For example, given a str of "Battle of the Vowels: Hawaii vs. Grozny" and a remove of "aeiou", the function should transform str to "Bttl f th Vwls: Hw vs. Grzny".
+# Justify any design decisions you make, and discuss the efficiency of your solution.
+
+def removeCharacters(string, removeChars):
+  for z in range(len(string[:])):
+    if string[z].lower() not in removeChars:
+      string += string[z]
+
+  return string
+
+
+print(removeCharacters("Battle of the Vowels: Hawaii vs. Grozny", "aeiou"))
+# O(n^2)
